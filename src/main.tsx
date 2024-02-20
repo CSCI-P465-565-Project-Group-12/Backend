a64533ae508c62ae2a270b1f899b319724dcc061
Pushkar Patil <pushkar@Pushkars-MacBook-Pro.local>
2024-02-20 17:26:38 -0500

    inital template

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+// import { Provider } from "react-redux";
+// import store from "./store/index.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-)
+  </BrowserRouter>
+);
---
+++ src/pages/VenueOwnerPage/VenueOwnerPage.tsx
@@ -0,0 +1,10 @@
+import "./VenueOwnerPage.css";
+
+const VenueOwnerPage = () => {
+  return (
+    <div className="VenueOwnerPage">
+      <h1>VenueOwnerPage</h1>
+    </div>
+  );
+};
+export default VenueOwnerPage;
